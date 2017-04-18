4332af87ff2f2380e66a33497a5a2593128a4636
Daymick <daymick@outlook.fr>
2017-04-18 02:13:23 +0200

    Add files via upload

---
+++ Text/client.lua
@@ -0,0 +1,3 @@
+AddEventHandler("playerSpawned", function(spawn)
+    TriggerEvent("chatMessage", "", { 0, 0, 0 }, "Bienvenue sur le Serveur pour vous faire spawn une voiture, faire /spawn nomvoiture bon jeu à vous ! ")
+end)
